549bea02f5b7b31585e5c1890fd9ac956e8e5e64
Freesca <checco1009@gmail.com>
2025-04-29 16:12:56 +0200

    graphical fixes om cocktails and places pages

--- frontend/src/app/pages/cocktails/cocktails.component.ts
+++ frontend/src/app/pages/cocktails/cocktails.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CocktailSorterComponent } from '../../components/cocktail-sorter/cocktail-sorter.component';
 import { CocktailsGridComponent } from '../../components/cocktails-grid/cocktails-grid.component';
 import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-cocktails',
   standalone: true,
-  imports: [CommonModule, CocktailSorterComponent, CocktailsGridComponent],
+  imports: [CommonModule, CocktailSorterComponent, CocktailsGridComponent, SearchBarComponent],
   templateUrl: 'cocktails.component.html',
   styleUrls: ['cocktails.component.scss'],
 })
