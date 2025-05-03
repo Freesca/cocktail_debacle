44cae36ed05cc9438b90d03942062777c6d3ae3b
Freesca <checco1009@gmail.com>
2025-05-03 19:37:31 +0200

    added edit profile section

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+export interface UpdateProfileDto {
+  username?: string;
+  email?: string;
+  oldPassword?: string;
+  newPassword?: string;
+  confirmPassword?: string;
+  consentData?: boolean;
+  consentSuggestions?: boolean;
+}
+
+export interface DeleteProfileDto {
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl = '/api/user';
+
+  constructor(private http: HttpClient) {}
+
+  updateProfile(data: UpdateProfileDto): Observable<any> {
+    return this.http.patch(`${this.baseUrl}/profile`, data, {
+      withCredentials: true
+    });
+  }
+
+  deleteAccount(data: DeleteProfileDto): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/profile`, {
+      body: data,
+      withCredentials: true
+    });
+  }
+
+}
+
